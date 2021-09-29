6b58ec455de910743bc66c3bb12fa08a6cb46b08
kaleumelo <kaleu_mt@hotmail.com>
2021-09-29 13:09:21 -0300

    solving messages

--- Questao 04.py
+++ Questao 04.py
@@ -1,1 +1,34 @@
-print('Digite as notas desse semestre. Ao terminar digite "-1"')
+i = 1
+lista = []
+notas_maiores = []
+notas_menores = []
+print('Digite as notas desse semestre. Ao final, digite "-1"')
+while(1):
+	notas = int(input('Nota ' + str(i) + ': '))
+	i += 1
+	if(notas == -1):
+		print('Acabaram as notas!')
+		break
+	else:
+		lista.append(notas)
+media = (sum(lista)/len(lista))
+print(len(lista))
+print(lista)
+lista.reverse()
+
+for i in range(len(lista)):
+    print(lista[i])
+
+for i in range(len(lista)):
+	if(lista[i] > media):
+		notas_maiores.append(lista[i])
+print(notas_maiores)
+
+for i in range(len(lista)):
+	if(lista[i] < 7):
+		notas_menores.append(lista[i])
+print(notas_menores)
+
+
+print(sum(lista))
+print(media)
